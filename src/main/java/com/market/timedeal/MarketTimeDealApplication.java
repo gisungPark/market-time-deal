7ed0c0d37203f34a0ba32a83e2d2d069c9a85f7d
MN4QYT6C7R <mn4qyt6c7r@baggiseong-ui-MacBookPro.local>
2023-02-26 21:18:36 +0900

    Initial commit

---
+++ src/main/java/com/market/timedeal/MarketTimeDealApplication.java
@@ -0,0 +1,13 @@
+package com.market.timedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketTimeDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketTimeDealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/timedeal/MarketTimeDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.timedeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketTimeDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
